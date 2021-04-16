f4d27ac3e49895dbe985a60bef22f0ed40b4339c
codewithavinash <avinashthakur.edu@gmail.com>
2021-04-16 14:54:03 +0530

    reverse Array O(n)

--- Java Files/src/com/company/ReversingArray1.java
+++ Java Files/src/com/company/ReversingArray1.java
@@ -1,2 +1,12 @@
-package com.company;public class ReversingArray1 {
+package com.company;
+
+public class ReversingArray1 {
+    public static void main(String[] args){
+        int Array[]={1,2,3,4,5};
+        int revArr[]=new int[5];
+        for (int i = Array.length-1, j = 0; i >=0 && j < Array.length; i--, j++){
+            revArr[j]=Array[i];
+            System.out.println(revArr[j]);
+        }
+    }
 }
