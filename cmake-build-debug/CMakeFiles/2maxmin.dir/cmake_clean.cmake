42949b6184ef2012a181e98090e3722212a415e7
codewithavinash <avinashthakur.edu@gmail.com>
2021-09-09 23:01:03 +0530

    Till Merge Linked List | Section 12 - 204

---
+++ cmake-build-debug/CMakeFiles/2maxmin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2maxmin.exe"
+  "2maxmin.exe.manifest"
+  "2maxmin.pdb"
+  "CMakeFiles/2maxmin.dir/main.cpp.obj"
+  "lib2maxmin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2maxmin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
